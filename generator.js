3d4e03f217ec97447ae4e2aab0099ef8a4ede881
Felix-Annaman <annaman819.fa@gmail.com>
2023-12-19 18:53:15 +0000

    css styling has been added and logic for generator password

--- generator.js
+++ generator.js
@@ -1,89 +1,92 @@
-/ Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+
+function getPasswordOptions() {
+
+    var length = prompt("Enter password length (8-128)");
+
+    if (length < 8 || length > 128) {
+
+        alert("Invalid length");
+
+        return;
+
+    }
  
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var lowercase = confirm("Include lowercase characters?");
+
+    var uppercase = confirm("Include uppercase characters?");
+
+    var numeric = confirm("Include numeric characters?");
+
+    var special = confirm("Include special characters? ($@%&*, etc.)");
  
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+    if (!lowercase && !uppercase && !numeric && !special) {
+
+        alert("At least one character type should be selected");
+
+        return;
+
+    }
  
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+    return {
+
+        length: length,
+
+        lowercase: lowercase,
+
+        uppercase: uppercase,
+
+        numeric: numeric,
+
+        special: special
+
+    };
+
+}
+ 
+function getRandom(str) {
+
+    return str.charAt(Math.floor(Math.random() * str.length));
+
+}
+ 
+function generatePassword() {
+
+    var options = getPasswordOptions();
+
+    if (!options) return "";
+ 
+    var password = "";
+
+    var characters = "";
+ 
+    if (options.lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+
+    if (options.uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (options.numeric) characters += "0123456789";
+
+    if (options.special) characters += "$@%&*";
+ 
+    for (var i = 0; i < options.length; i++) {
+
+        password += getRandom(characters);
+
+    }
+ 
+    return password;
+
+}
+ 
+var generateBtn = document.querySelector('#generate');
+ 
+function writePassword() {
+
+    var password = generatePassword();
+
+    var passwordText = document.querySelector('#password');
+ 
+    passwordText.value = password;
+
+}
+ 
+generateBtn.addEventListener('click', writePassword);
